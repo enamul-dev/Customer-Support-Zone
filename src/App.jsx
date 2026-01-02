c8275ce40c4b1cb522b05d563feddadc82a98f05
enamul <enamul.dev01@gmail.com>
2026-01-02 09:34:29 +0600

    progress and resolved section added

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,28 @@ function App() {
               <li>Download</li>
               <li>Contact</li>
             </ul>
-            <button className='bg-gradient-to-r from-indigo-500 to-indigo-600 text-white px-2 py-2 rounded'><i class="fa-solid fa-plus pr-2 fa-xs"></i>New Ticket</button>
+            <button className='bg-linear-to-r from-indigo-500 to-indigo-600 text-white px-2 py-2 rounded'><i class="fa-solid fa-plus pr-2 fa-xs"></i>New Ticket</button>
           </div>
         </nav>
       </header>
+
+      {/* In progress and Resolved section */}
+
+      <section>
+        <div className='flex flex-col md:flex-row container justify-between gap-7 py-16'>
+          <div className='bg-linear-to-r from-indigo-400 to-indigo-600 text-white rounded w-200 h-70 flex flex-col justify-center items-center'>
+            <h1 className='text-2xl'>In-Progress</h1>
+            <p className='text-4xl'>0</p>
+          </div>
+          <div className='bg-linear-to-r from-green-400 to-green-600 text-white  rounded w-200 h-70 flex flex-col justify-center items-center'>
+            <h1 className='text-2xl'>Resolved</h1>
+            <p className='text-4xl'>0</p>
+        </div>
+        </div>
+        
+      </section>
+
+
     </>
   )
 }
